e61a3bb427afb574f74efa8ef715980ab703a4e5
Gleison Nascimento <gleison.nascimento1997@gmail.com>
2018-10-31 03:01:12 -0300

    versão com tela de login e cliente

---
+++ src/controller/FXMLLoginController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author gleis
+ */
+public class FXMLLoginController implements Initializable {
+
+    @FXML
+    private Button btnSair;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    private PasswordField txtSenha;
+
+    @FXML
+    private Button btnEntrar;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- src/main/Principal.java
+++ src/main/Principal.java
@@ -6,24 +6,45 @@
 package main;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
  * @author paste
  */
 public class Principal extends Application {
-    
+
+    private static double xOffset = 0;
+    private static double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLHome.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.centerOnScreen();
+        stage.setResizable(false);
+        stage.setTitle("Principal");
+        stage.getIcons().add(new javafx.scene.image.Image("icons/padrao.png"));
+        // mover tela
+
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = stage.getX() - event.getScreenX();
+            yOffset = stage.getY() - event.getScreenY();
+        });
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        });
         stage.show();
     }
 
@@ -33,5 +54,5 @@ public class Principal extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
